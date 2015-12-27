aad60dfc1b35bf20caabb5387102a49d30389049
belieffs22 <beliefs22@gmail.com>
2015-12-27 00:42:34 -0500

    removed files , modified class

--- prime.py
+++ prime.py
@@ -1,88 +1,82 @@
-
+import pickle
 class Prime:
-    """A class for generating the first n Primes. If no number is given it will
-    generate the first 10,000 Primes from list. 
+    """A class for user first 10,000 Primes in various math problems."""
+    def __init__(self):
+        
+        self.count = 0 # used to reset iterator
+        self.index = 0 # used to reset iterator
+        primefile = open(
+            '/home/beliefs22/python/numberclasses/Data/primetuples','r')
+        self.primes = pickle.load(primefile)
+        primefile.close()
 
-    Attributes:
-        primes (dict)= where the keys are the prime number
-            stored as int, and the value is the value of that
-            prime stored as int
-        ordred (list of ints)= A list containing all primes in order by value
-            smalled to largest
-        index (int) = used for interation initializd to 0
-        count (int) = used for generating one prime at a Prime initlized to 0
-
-"""
-    def __init__(self,num = None):
-        """
-            Args: None
-        """
-        self.count = 0
-        self.index = 0
-        self.primes = {}
-        self.ordered = []
-        if num == None:
-            __myfile = open("primes.txt",'r')
-            for line in __myfile:           
-                temp = line.rstrip("\n")
-                self.primes[temp] = int(temp)
-                self.ordered.append(int(temp))
-            __myfile.close()
-        else:
-            place = 0
-            __myfile = open("primes.txt",'r')            
-            for line in __myfile:
-                if place > num:
-                    break
-                temp = line.rstrip("\n")
-                self.primes[int(temp)] = int(temp)
-                self.ordered.append(int(temp))
-                place = place + 1
-            __myfile.close()
-
-    def maxPrime(self):
-        """Returns the maximum Prime this Prime object
-        Contains
-
-        Args:
-            None
-
-        Returns:
-                Int representing max Prime
-        """
-        return self.ordered[len(self.ordered)-1]
-                
-
-    def isPrime(self,num):
-        """Checks if given number is Prime
+    def is_prime(self,num):
+        """Checks if given number is Prime.
 
         Args:
             num (int): Number to check Primality
 
         Returns:
-            True if num is Prime, False otherwise
+            bool: True if num is Prime, False otherwise
+            
         """
         if num == 1:
             return True
         else:
             return num in self.primes
 
-    def genPrime(self,length):
-        """Return a generator object of primes a given number in length
+    def gen_Prime(self,length):
+        """Return a list of first length primes.
 
         Args:
-            length (int): how many primes the generator should create
+            length (int): number of primes to find
 
-        Yields:
-            Generator of the first "length" primes
+        Returns:
+            tuple: tuple of the first  "length" primes
+            
+        """
+    
+        return self.primes[:length]
+
+    def prime_generator_value(self, maxval=None):
+        """Returns generator of primes up to a max value.
+
+            Args:
+                maxval (int): max value of prime to generate. Defaults to max
+                    prime available if no maxvalue given
+
+            Yields:
+                int: next prime in sequence
+
         """
         
-        for i in xrange(length):
-            yield self.ordered[i]           
-        
+        if maxval == None: #if no value given
+            maxval = self.primes[len(self.primes)-1]
+        pos = 0
+        while self.primes[pos] <= maxval:
+            yield self.primes[pos]
+            pos += 1
+            
+    def prime_generator_count(self, maxcount=None):
+        """Returns generator of primes up to max count.
 
+        Args:
+            maxcount (int): maximum number of primes to generator. Defaults to
+                all primes available if no maxcount is given
+
+        Yields:
+            int: next prime in sequence
+
+        """
+        if maxcount == None:
+            maxcount = len(self.primes)
+        count = 0
+        while  count < maxcount:
+            yield self.primes[count]
+            count += 1
+            
     def getPrime(self,num):
-        """Gets the ith prime where i is given number
+        """Return request prime. I.E. getPrime(number.
 
         Args:
             num (int): the ith prime to find
@@ -90,78 +84,70 @@ class Prime:
         Returns:
             int represeting the ith Prime
         """
-        return self.ordered[num-1]
+        return self.primes[num-1]
 
     def nextPrime(self):
         """Returns the next Prime based on value of self.count
 
-        Args: None
-
         Returns:
             int represeting the next prime in sequence
         """
-        temp = self.ordered[self.count]
+        temp = self.primes[self.count]
         self.count += 1
         return temp
-    def reset(self,c = False,i = False):
-        """Resets self.count and self.index to 0. To begin new count or iteration
+    
+    def reset(self,count = False,index = False):
+        """Resets self.count and self.index to 0.
+            Used for interation and to generated next prime without a generator
 
         Args:
-            c (boolean) :Defaults to false. Set to True if you only want to only reset Count
-            i (bolean)  :Defaults to false. Set to True if you only want to only reset index
-            **If you want to reset both, leave both as false**
+            count (boolean) :Defaults to fa lse. Set to True if you only
+                want to only reset Count
+            index (bolean)  :Defaults to false. Set to True if you only want
+                to only reset index
+                **If you want to reset both, leave both as false**
 
         """
-        if c == False and i == False:
-            c,i = True,True
-        if self.count > 0 and c:
+        if count == False and index == False:
+            count,index = True,True
+        if self.count > 0 and count:
             self.count = 0
-        if self.index and i > 0:
+        if self.index and index > 0:
             self.index = 0
 
     def __iter__(self):
-        """Iterates over entire list of primes"""
+        """Iterates over entire list of primes. Expects user to stop."""
         return self
-    
     def next(self):
-        if self.index == len(self.ordered)-1:
+        if self.index == len(self.primes)-1:
             raise StopIteration
-        else:
-            temp = self.index
-            self.index = self.index +1
-            return self.ordered[temp]
-
+        temp = self.index
+        self.index = self.index +1
+        return self.primes[temp]
+    
 
 def main():
     t = Prime()
-    iter(t)
     print t.getPrime(50)
     
     for i in range(100):
         print t.nextPrime()
 
-    for prime in t:
+    first_100 = t.prime_generator_count(100)
+    for prime in first_100:
         print prime
+    
+    for prime in t:
         if prime > 541:
             break
-    t.reset(c=False,i=True)
-    for i in range(100):
-        print t.nextPrime()
-    for prime in t:
         print prime
-        if prime > 541:
-            break
-    t.reset()
-    for prime in t:
-        print t.nextPrime()
-        print prime
-        if prime > 541:
-            break
 
+    greater_than = t.prime_generator_value(541)
+    for prime in greater_than:
+        print prime
 
 if __name__ == '__main__':
     main()
-
 
 
 
